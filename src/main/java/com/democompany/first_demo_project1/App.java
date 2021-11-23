cacc51b6d7d3d212bae19fcb44c78b751dda5283
rdmanju <rmanjudharani007@gmail.com>
2021-11-23 18:03:10 +0530

    added initial commit

---
+++ src/main/java/com/democompany/first_demo_project1/App.java
@@ -0,0 +1,13 @@
+package com.democompany.first_demo_project1;
+
+/**
+ * Hello world shiva!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World shiva!" );
+    }
+}
